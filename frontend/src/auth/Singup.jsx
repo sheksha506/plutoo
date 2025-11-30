7807005011504fc39fb2bab26bc2ba15d3e4ab56
sheksha506 <sekshavali77@gmail.com>
2025-11-30 23:51:22 +0530

    navigate added

--- frontend/src/auth/Singup.jsx
+++ frontend/src/auth/Singup.jsx
@@ -22,11 +22,7 @@ const Singup = () => {
     e.preventDefault();
 
     try {
-      await axios.post(
-  "/api/signup",
-  form
-);
-
+      await axios.post("/api/signup", form);
 
       navigate("/");
     } catch (error) {
@@ -36,7 +32,6 @@ const Singup = () => {
 
   return (
     <div className="h-screen bg-gray-100 flex flex-col md:flex-row items-center justify-center px-5 md:px-10">
-      
       {/* Title & Subtitle */}
       <div className="w-full md:flex-1 mb-8 md:mb-0 text-center md:text-left">
         <h1 className="text-4xl md:text-5xl font-bold text-blue-600">
@@ -50,7 +45,8 @@ const Singup = () => {
       {/* Form */}
       <div className="w-full md:flex-1 flex justify-center">
         <form
-          onSubmit={detailsSubmit} method="POST"
+          onSubmit={detailsSubmit}
+          method="POST"
           className="flex flex-col w-full sm:w-4/5 md:w-[70%] bg-white shadow-lg p-6 sm:p-8 rounded-xl"
         >
           <h2 className="text-2xl font-semibold text-gray-800 text-center mb-6">
@@ -90,9 +86,12 @@ const Singup = () => {
 
           <p className="text-center text-sm text-black mt-4">
             Already have an account?{" "}
-            <span className="text-blue-500 cursor-pointer hover:underline">
+            <button
+              onClick={() => navigate("/signup")}
+              className="text-blue-500 cursor-pointer hover:underline"
+            >
               Login
-            </span>
+            </button>
           </p>
         </form>
       </div>
